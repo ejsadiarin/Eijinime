8a06851a87a940df678041807746470400dcb8c2
ejsadiarin <ejsadiarin@gmail.com>
2023-04-18 16:03:34 +0800

    working fetch, TODO: resolve type ReactNode error

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { ANIME } from '@consumet/extensions'
+import { ANIME, ITitle} from '@consumet/extensions'
 
 
 // react query ?
+type Anime = {
+  id: string,
+  title: string | ITitle,
+  image: string | undefined,
+  url: string | undefined,
+}
 
 function App() {
+  const [topAiring, setTopAiring] = useState<Anime[]>([]);
+
   const gogoAnime = new ANIME.Gogoanime();
-  const fetchTopAiring  = gogoAnime.fetchTopAiring().then(data => console.log);
-
   
+  useEffect(() =>{
+    async function getTopAiringAnime() {
+      const response = await gogoAnime.fetchTopAiring();
 
+      const topAiringData = await Promise.all(response.results.map(async (obj) => {
+        return {
+        id: obj.id,
+        title: obj.title,
+        image: obj.image,
+        url: obj.url,
+        }
+      }))
+      setTopAiring(topAiringData);
+
+    }
+
+    getTopAiringAnime();
+  }, [])
 
   return (
     <div className="App">
-      <h1>test</h1>
+      <h1>Top Airing Anime</h1>
+      {topAiring.map((anime) => (
+        <ul>
+          <li key={anime.id}>{anime.title}
+          <img src={anime.image} alt={anime.title} />
+          <a href={anime.url}>{anime.title}</a>
+          </li>
+        </ul>
+      ))}
+
     </div>
-  )
+  );
 }
 
 export default App
